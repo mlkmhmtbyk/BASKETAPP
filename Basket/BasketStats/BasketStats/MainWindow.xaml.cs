90309d2edf5bf8432a3c5a48f97aba17f6fef0bc
mlkmhmtbyk <biyikm16@itu.edu.tr>
2019-12-05 03:24:52 +0300

    final statistic app

--- Basket/BasketStats/BasketStats/MainWindow.xaml.cs
+++ Basket/BasketStats/BasketStats/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Globalization;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Configuration;
 
 namespace BasketStats
 {
@@ -26,18 +29,54 @@ namespace BasketStats
             InitializeComponent();
         }
         public DateTime startTime;
-
+        public static string ConnStr = ConfigurationManager.ConnectionStrings["MySql"].ToString();
+        public MySqlConnection con = new MySqlConnection(ConnStr);
 
         private void BackGround_Click(object sender, RoutedEventArgs e)
         {
+            Dictionary<string, object> statDict = new Dictionary<string, object>();
+
             double X = Mouse.GetPosition(this).X;
             double Y = Mouse.GetPosition(this).Y;
             string playerId = PlayerTxt.Text;
             PlayerTxt.Clear();
-            var isHome = Home.IsChecked;
+            string score = ScoreTxt.Text;
+            ScoreTxt.Clear();
+            int isHome;
+            if (Home.IsChecked == true)
+            {
+                isHome = 1;
+            }
+            else
+            {
+                isHome = 0;
+            }
             DateTime currentTime = DateTime.Now;
             TimeSpan scoreTime = currentTime.Subtract(startTime);
-            scoreTime.ToString("h'h 'm'm 's's'");
+            string strScoreTime = scoreTime.ToString("h'h 'm'm 's's'");
+
+            statDict.Add("playerId", playerId);
+            statDict.Add("score", score);
+            statDict.Add("X", X);
+            statDict.Add("Y", Y);
+            statDict.Add("isHome", isHome);
+            statDict.Add("scoreTime", strScoreTime);
+
+            try
+            {
+                con.Open();
+            }
+            catch(MySqlException ex)
+            {
+                string message = ex.Message;
+            }
+
+            string query = "INSERT INTO AppStatistic(PlayerID,Score,PositionX,PositionY,IsHome,ScoreTime) VALUES(" + "'" +statDict["playerId"] + "'" + "," + "'" + statDict["score"]+ "'" + "," + "'" + statDict["X"]+ "'" + "," + "'" + statDict["Y"]+ "'" + "," + "'" + statDict["isHome"] + "'" + "," + "'" + strScoreTime + "'" +")" ;
+            MySqlCommand cmd = new MySqlCommand(query, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+
         }
 
         public void start_Click(object sender, RoutedEventArgs e)
@@ -50,5 +89,7 @@ namespace BasketStats
         {
             DateTime finishTime = DateTime.Now;
         }
+
+        
     }
 }
